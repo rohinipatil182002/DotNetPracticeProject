938e8b25d49c57f0d89dfc925f3ca14dd0967990
rohinipatil182002 <rohinipatil182002@gmail.com>
2026-03-26 18:33:24 +0530

    add some tables and add the master data in thta tables

---
+++ dotNetPracticeProject/Data/Currency.cs
@@ -0,0 +1,9 @@
+﻿namespace dotNetPracticeProject.Data
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
