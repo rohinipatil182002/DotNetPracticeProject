eefae27b4b2b00f59d852a668a84e7faee9dbbae
rohinipatil182002 <rohinipatil182002@gmail.com>
2026-03-27 11:18:04 +0530

    get all records in entity framework core linQ tolist() & tolistasync()

---
+++ dotNetPracticeProject/Controllers/LanguageController.cs
@@ -0,0 +1,32 @@
+﻿using dotNetPracticeProject.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotNetPracticeProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LanguageController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public LanguageController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet("get-all-languages")]
+        public async Task<IActionResult> GetLanguage()
+        {
+            // var result = _appDbContext.Languages.ToList();
+            //var result = (from languages in _appDbContext.Languages select languages).ToList();
+
+            var result = await _appDbContext.Languages.ToListAsync();
+            //var result = await (from languages in _appDbContext.Languages select languages).ToListAsync();
+
+
+            return Ok(result);
+        }
+    }
+}
--- dotNetPracticeProject/Migrations/20260327051859_addBookPrice.Designer.cs
+++ dotNetPracticeProject/Migrations/20260327051859_addBookPrice.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using dotNetPracticeProject.Data;
 
@@ -11,9 +12,11 @@ using dotNetPracticeProject.Data;
 namespace dotNetPracticeProject.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260327051859_addBookPrice")]
+    partial class addBookPrice
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
